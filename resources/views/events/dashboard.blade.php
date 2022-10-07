63d51e3f207836732878ccdc37f300dd1123b320
mateus a <mateusaugustot17@gmail.com>
2022-10-06 23:52:44 -0300

    initial commit

---
+++ resources/views/events/dashboard.blade.php
@@ -0,0 +1,82 @@
+@extends('layouts.main')
+
+@section('title','Dassboard')
+
+
+@section('content')
+<div class="col-md-10 offset-md-1 dashboard-title-container">
+    <h1>Meus Eventos</h1>
+</div>
+<div class="col-md-10 offset-md-1 dashboard-events-container">
+    @if(count($events)>0)
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Participantes</th>
+                <th scope="col">Ações</th>
+               
+            </tr>
+        </thead>
+    
+    <tbody>
+        @foreach($events as $event)
+        <tr>
+            <td scrop="row">{{$loop->index +1}}</td>
+            <td><a href="/events/{{$event->id}}">{{$event->title}}</a></td>
+            <td>{{count($event->users)}}</td>
+            <td>
+                <a href="/events/edit/{{$event->id}}" class="btn btn-info edit-btn"><ion-icon name="create-outline"></ion-icon> Editar</a> 
+                <form action="/events/{{$event->id}}" method="POST">
+                @csrf
+                @method('delete')
+                    <button type="submit" class="btn btn-danger delete-btn"><ion-icon name="trash-outline"></ion-icon>Deletar</button>
+                </form>
+
+                </td>
+        </tr>
+        @endforeach
+    </tbody>
+    </table>
+    
+    @else
+    <p>Voccê não tem eventos <a href="/events/create">Criar Evento</a></p>
+    @endif
+</div>
+<div class="col-md-10 offset-md-1 dashboard-title-container">
+    <h1>Eventos que estou participando</h1>
+</div>
+<div class="col-md-10 offset-md-1 dashboard-title-container">
+@if(count($eventsAsParticipant) > 0)
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Participantes</th>
+                <th scope="col">Ações</th>
+               
+            </tr>
+        </thead>
+    
+    <tbody>
+        @foreach($eventsAsParticipant as $event)
+        <tr>
+            <td scrop="row">{{$loop->index +1}}</td>
+            <td><a href="/events/{{$event->id}}">{{$event->title}}</a></td>
+            <td>{{count($event->users)}}</td>
+            <td>
+                <a href="#">Sair do evento</a>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+    </table>
+
+@else
+<p>Você ainda não está participando de nenhum evento,<a href="/">Veja todos os eventos</p>
+@endif
+
+</div>
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title')</title>
+        <!-- -->
+        <!-- -->
+        <!--bootstrap -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <!--FONTS -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https: //fonts.googleapis.com/css2? family= Lato:ital,wght@0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900 & display=swap" rel="stylesheet">
+        <!-- CSS -->
+        <link rel="stylesheet" href="/css/style.css">
+        <!--JAVASCRIP -->
+        <script src="/js/scripts.js"></script>
+        
+    </head>
+    <body>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="collapse navbar-collapse" id="navbar">
+                    <a href="/" class="navbar-brand">
+                    <img src="/img/logo2.jpg" alt="HDC">
+                    </a>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="/" class="nav-link">Eventos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="events/create" class="nav-link">Criar Eventos</a>
+                        </li>
+                        @auth
+                         <li class="nav-item">
+                            <a href="/dashboard" class="nav-link">Meus Eventos</a>
+                        </li>
+                        <li class="nav-item">
+                            <form action="/logout" method="POST">
+                                @csrf
+                                <a href="/logout"
+                                class="nav-link"
+                                onclick="event.preventDefault();
+                                    this.closest('form').submit();"
+                                >
+                                
+                            Sair
+                            </a>
+                            </form>
+
+                        </li>
+                        @endauth
+                        @guest
+                        <li class="nav-item">
+                            <a href="/login" class="nav-link">Entrar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/register" class="nav-link">Cadastrar</a>
+                        </li>
+                        @endguest
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <main>
+            <div class="container-fluid">
+                <div class="row">
+                    @if(session('msg'))
+                    <p class="msg">{{session('msg')}}</p>
+                    @endif
+                    @yield('content')
+                </div>
+            </div>
+
+        </main>
+    <footer>
+        <p>Projeto laravel &copy; 2022</p>
+    </footer>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    </body>
+</html>
